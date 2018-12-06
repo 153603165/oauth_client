11d03a950b1383010774fb16b22fbb8febf3295b
Ex＇7 <153603165@qq.com>
2018-12-06 17:18:29 +0800

    修改启动类

--- src/main/java/com/kfwl/oauth_client/App.java
+++ src/main/java/com/kfwl/oauth_client/App.java
@@ -1,25 +1,12 @@
 package com.kfwl.oauth_client;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.context.request.RequestContextListener;
 
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
-@EnableResourceServer
-public class App extends SpringBootServletInitializer {
-
-	@Bean
-	public RequestContextListener requestContextListener() {
-		return new RequestContextListener();
-	}
-
+public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
