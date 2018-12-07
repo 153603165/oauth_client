5987b072ffe62bd7045b777e529ee969a3e002d3
Ex＇7 <153603165@qq.com>
2018-12-07 10:22:43 +0800

    修改oauth客户端例子代码

---
+++ src/main/java/com/kfwl/oauth_client/OauthClientApp.java
@@ -0,0 +1,15 @@
+package com.kfwl.oauth_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class OauthClientApp {
+	public static void main(String[] args) {
+		SpringApplication.run(OauthClientApp.class, args);
+	}
+}
