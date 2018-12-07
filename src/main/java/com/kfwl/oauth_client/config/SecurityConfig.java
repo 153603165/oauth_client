4466f2c6c71d41b2e656792d5bbb00ce0f107669
Ex＇7 <153603165@qq.com>
2018-12-07 11:22:08 +0800

    优化授权。

--- src/main/java/com/kfwl/oauth_client/config/SecurityConfig.java
+++ src/main/java/com/kfwl/oauth_client/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableOAuth2Sso
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 获取授权服务器的权限控制方法
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/test").permitAll().anyRequest()
